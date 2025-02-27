7f850a4d391a5949651319f597089c6a6c59216d
Fabio Carpinato <fabiocarpinato@outlook.it>
2025-02-27 12:12:21 +0100

    feat: cli app can now send messages and show responses

---
+++ packages/cli/src/app.rs
@@ -0,0 +1,107 @@
+use crossterm::event::{self, Event, KeyCode, KeyEventKind};
+use ratatui::{
+    prelude::*,
+    widgets::{Block, Borders, List, ListItem, Paragraph},
+    DefaultTerminal,
+};
+use std::{
+    io::{Read, Result, Write},
+    net::TcpStream,
+    str,
+};
+use tui_input::{Input, InputRequest};
+
+pub struct App {
+    input: Input,
+    messages: Vec<String>,
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            input: Input::default(),
+            messages: vec![],
+        }
+    }
+}
+
+impl App {
+    pub fn run(mut self, mut terminal: DefaultTerminal, mut stream: TcpStream) -> Result<()> {
+        loop {
+            terminal.draw(|frame| self.draw(frame))?;
+            if let Event::Key(key) = event::read()? {
+                match key.code {
+                    KeyCode::Enter => {
+                        let input = self.input.value();
+
+                        let _ = stream.write_all(input.as_bytes());
+                        let mut buffer = [0; 1024];
+                        let bytes_read = stream.read(&mut buffer)?;
+                        let response = str::from_utf8(&buffer[..bytes_read]).unwrap();
+
+                        self.messages.push(format!("Input: {}", input.to_string()));
+                        self.messages
+                            .push(format!("Response: {}", response.to_string()));
+
+                        let req = InputRequest::DeleteLine;
+                        self.input.handle(req);
+                    }
+                    KeyCode::Char(to_insert) => {
+                        let req = InputRequest::InsertChar(to_insert);
+                        self.input.handle(req);
+                    }
+                    KeyCode::Backspace => {
+                        let req = InputRequest::DeletePrevChar;
+                        self.input.handle(req);
+                    }
+                    KeyCode::Esc => {
+                        break Ok(());
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    fn draw(&self, f: &mut Frame) {
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(2)
+            .constraints(
+                [
+                    Constraint::Length(1),
+                    Constraint::Length(3),
+                    Constraint::Min(1),
+                ]
+                .as_ref(),
+            )
+            .split(f.area());
+
+        let width = chunks[0].width.max(3) - 3;
+
+        let scroll = self.input.visual_scroll(width as usize);
+        let input = Paragraph::new(self.input.value())
+            .style(Style::default().fg(Color::Yellow))
+            .scroll((0, scroll as u16))
+            .block(Block::default().borders(Borders::ALL).title("Input"));
+
+        f.render_widget(input, chunks[1]);
+        f.set_cursor_position((
+            chunks[1].x + ((self.input.visual_cursor()).max(scroll) - scroll) as u16 + 1,
+            chunks[1].y + 1,
+        ));
+
+        let messages: Vec<ListItem> = self
+            .messages
+            .iter()
+            .enumerate()
+            .map(|(i, m)| {
+                let content = vec![Line::from(Span::raw(format!("{}: {}", i, m)))];
+                ListItem::new(content)
+            })
+            .collect();
+        let messages =
+            List::new(messages).block(Block::default().borders(Borders::ALL).title("Messages"));
+        f.render_widget(messages, chunks[2]);
+    }
+}
--- packages/cli/src/main.rs
+++ packages/cli/src/main.rs
@@ -1,16 +1,12 @@
+mod app;
+
+use app::App;
 use clap::Parser;
-use crossterm::event::{self, Event};
-use ratatui::{
-    layout::{Constraint, Direction, Layout},
-    widgets::{Block, Borders, Paragraph},
-    DefaultTerminal, Frame,
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use std::{
+    io::{self},
+    net::TcpStream,
 };
-use widgets::command_input_widget::CommandInputWidget;
-use std::{io::{self, Read, Result, Write}, net::TcpStream, str};
-
-mod widgets;
-
-use crate::widgets::command_input_widget;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -28,66 +24,16 @@ struct Args {
 fn main() -> io::Result<()> {
     let args = Args::parse();
 
-    match color_eyre::install() {
-        Ok(_) => {
-            //
-        }
-        Err(_err) => {
-            //
-        }
-    }
+    color_eyre::install().expect("Error while installing color_eyre");
+    enable_raw_mode()?;
+
+    let stream = TcpStream::connect(args.url)?;
 
     let terminal = ratatui::init();
-    let result = run(terminal);
+    let result = App::default().run(terminal, stream);
+
     ratatui::restore();
-
-    let mut stream = TcpStream::connect(args.url)?;
-
-    let _ = stream.write_all(b"+PING\r\n");
-
-    let mut buffer = [0; 1024];
-
-    let bytes_read = stream.read(&mut buffer)?;
-
-    let response = str::from_utf8(&buffer[..bytes_read]).unwrap();
-
-    println!("{}", response);
-
+    disable_raw_mode()?;
 
     result
-}
-
-fn run(mut terminal: DefaultTerminal) -> Result<()> {
-    loop {
-        terminal.draw(render)?;
-
-        if matches!(event::read()?, Event::Key(_)) {
-            break Ok(());
-        }
-    }
-}
-
-fn render(frame: &mut Frame) {
-    let outer_layout = Layout::default()
-        .direction(Direction::Vertical)
-        .constraints(vec![Constraint::Percentage(90), Constraint::Percentage(10)])
-        .split(frame.area());
-
-    let inner_layout = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints(vec![Constraint::Percentage(25), Constraint::Percentage(75)])
-        .split(outer_layout[1]);
-
-    frame.render_widget(
-        Paragraph::new("outer 0").block(Block::new().borders(Borders::ALL)),
-        outer_layout[0],
-    );
-    frame.render_widget(
-        Paragraph::new("inner 0").block(Block::new().borders(Borders::ALL)),
-        inner_layout[0],
-    );
-    frame.render_widget(
-        CommandInputWidget::new(),
-        inner_layout[1],
-    );
 }
